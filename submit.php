475ea74ec6aae6aa7513433e988501b1779bfd19
Josip Kilić <neksuses@gmail.com>
2023-07-02 16:02:50 +0200

    New PHPMailer

--- submit.php
+++ submit.php
@@ -29,9 +29,9 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require 'PHPMailer/PHPMailer.php';
-    require 'PHPMailer/SMTP.php';
-    require 'PHPMailer/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+    require 'PHPMailer/src/Exception.php';
 
     error_reporting(0);
 
