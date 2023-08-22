9bb992b0966046d39d9eb7b59bd54e842d2c6507
Josip Kilić <neksuses@gmail.com>
2023-08-22 15:21:08 +0200

    Add beach, more changes

--- js/photos.js
+++ js/photos.js
@@ -2,6 +2,7 @@
 /// PORTRAIT ALBUMS
 ///
 const apartmanDvoristePortrait = 'https://photos.app.goo.gl/6kDWDpqSAY6werKH6';
+const apartmanPlazaPortrait = 'https://photos.app.goo.gl/XXsEkXW95BpmtyiH7';
 const apartmanMarijaPortrait = 'https://photos.app.goo.gl/j5ZDHULGkKQFvEtEA';
 const apartmanJosipPortrait = 'https://photos.app.goo.gl/wNmeGtvuWEkNiwr6A';
 
@@ -9,6 +10,7 @@ const apartmanJosipPortrait = 'https://photos.app.goo.gl/wNmeGtvuWEkNiwr6A';
 /// LANDSCAPE ALBUMS
 ///
 const apartmanDvoristeLandscape = 'https://photos.app.goo.gl/1i464C6NPaSrtooA7';
+const apartmanPlazaLandscape = 'https://photos.app.goo.gl/ygyTCdYyot5ULm5o6';
 const apartmanMarijaLandscape = 'https://photos.app.goo.gl/BtUPxF2SHJ3B1NFd9';
 const apartmanJosipLandscape = 'https://photos.app.goo.gl/A7GCtecZhnMsJoCc7';
 
@@ -25,6 +27,7 @@ function isMobileViewport() {
 const buttonMarija = document.getElementById("buttonMarija");
 const buttonJosip = document.getElementById("buttonJosip");
 const buttonDvoriste = document.getElementById("buttonDvoriste");
+const buttonPlaza = document.getElementById("buttonPlaza");
 
 const iconMarija = document.getElementById("iconMarija");
 const photosMarija = document.getElementById("photosMarija");
@@ -34,13 +37,26 @@ const iconJosip = document.getElementById("iconJosip");
 const photosJosip = document.getElementById("photosJosip");
 const textButtonJosip = document.getElementById("textButtonJosip");
 
+const iconDvoriste = document.getElementById("iconDvoriste");
 const photosDvoriste = document.getElementById("photosDvoriste");
+const textButtonDvoriste = document.getElementById("textButtonDvoriste");
+
+const iconPlaza = document.getElementById("iconPlaza");
+const photosPlaza = document.getElementById("photosPlaza");
+const textButtonPlaza = document.getElementById("textButtonPlaza");
 
 // Check if the viewport width corresponds to a mobile device
 if (isMobileViewport()) {
     // Set the link's destination for portrait gallery
+    buttonPlaza.href = apartmanPlazaPortrait;
+    iconPlaza.href = apartmanPlazaPortrait;
+    photosPlaza.href = apartmanPlazaPortrait;
+    textButtonPlaza.href = apartmanPlazaPortrait;
+
     buttonDvoriste.href = apartmanDvoristePortrait;
+    iconDvoriste.href = apartmanDvoristePortrait;
     photosDvoriste.href = apartmanDvoristePortrait;
+    textButtonDvoriste.href = apartmanDvoristePortrait;
 
     buttonMarija.href = apartmanMarijaPortrait;
     iconMarija.href = apartmanMarijaPortrait;
@@ -53,8 +69,15 @@ if (isMobileViewport()) {
     textButtonJosip.href = apartmanJosipPortrait;
 } else {
     // Set the link's destination for landscape gallery
+    buttonPlaza.href = apartmanPlazaLandscape;
+    iconPlaza.href = apartmanPlazaLandscape;
+    photosPlaza.href = apartmanPlazaLandscape;
+    textButtonPlaza.href = apartmanPlazaLandscape;
+
     buttonDvoriste.href = apartmanDvoristeLandscape;
+    iconDvoriste.href = apartmanDvoristeLandscape;
     photosDvoriste.href = apartmanDvoristeLandscape;
+    textButtonDvoriste.href = apartmanDvoristeLandscape;
 
     buttonMarija.href = apartmanMarijaLandscape;
     iconMarija.href = apartmanMarijaLandscape;
